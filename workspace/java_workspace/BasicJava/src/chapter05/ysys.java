8d9a1c014c16f0541b75bdd5c84d5a0c2d330026
NAYEONSEUNG <58206041+NAYEONSEUNG@users.noreply.github.com>
2019-12-02 16:50:40 +0900

    싱글톤,this, 등등....

---
+++ workspace/java_workspace/BasicJava/src/chapter05/ysys.java
@@ -0,0 +1,27 @@
+package chapter05;
+
+import java.util.Scanner;
+
+public class ysys {
+
+	public void main(String []args) {
+		
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.print("입력>>");
+	 int num1 = 1; //첫번째항
+	 int num2 = 1; //두번째항
+	 int num3;	   //결과값
+	
+	 for(int i = 1; i < ; i ++) {
+
+		 num3 = num1 + num2;
+		 num1 = num2;
+		 num2 = num3;
+		 
+	  }//for
+	 
+	 
+	}//main
+	
+}//class
